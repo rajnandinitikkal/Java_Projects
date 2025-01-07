295ecc597e619581d65eb0a08b4cd00c662b20b2
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-07 15:56:03 +0530

    add new project

--- Form_using_Swings/form/src/main/java/com/example/Main.java
+++ Form_using_Swings/form/src/main/java/com/example/Main.java
@@ -1,9 +1,7 @@
 package com.example;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        @SuppressWarnings("unused")
-        MyWindow window = new MyWindow();
     }
 }

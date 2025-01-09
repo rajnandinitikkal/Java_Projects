e22085e3e9cece6d31b769e50a3ae4c2aac835a0
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-09 13:08:08 +0530

    add new form project

---
+++ Form_using_Swings/form/src/main/java/com/example/Window.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+    public Window()
+    {
+        super.setTitle("Form Using Java Jrame");
+        super.setSize(400, 400);
+        super.setLocation(600, 50);
+        super.setVisible(true);
+        
+    }
+}

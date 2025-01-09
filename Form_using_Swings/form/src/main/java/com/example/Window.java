fb23036051954a71627aea2f318d99692677c052
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-09 16:00:56 +0530

    add new project

--- Form_using_Swings/form/src/main/java/com/example/Window.java
+++ Form_using_Swings/form/src/main/java/com/example/Window.java
@@ -1,33 +1,80 @@
 package com.example;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Window extends JFrame {
 
     private JLabel heading;
     Font font = new Font("", Font.BOLD, 30);
+    
+    private JPanel mainPanel;
+    private JLabel nameLabel,passwordLabel;
+    private JPasswordField passwordField;
+    private JButton button1,button2;
+        private Component nameTextField;
+    
+        public Window()
+        {
+            super.setTitle("Form Using Java Jrame");
+            super.setSize(500, 400);
+            super.setLocation(600, 50);
+           
+            super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.createGUI();
+    
+            super.setVisible(true);
+        }
+    
+        public void createGUI(){
+    
+            this.setLayout(new BorderLayout());
+            heading=new JLabel("First Form...");
+            heading.setFont(font);
+            heading.setHorizontalAlignment(JLabel.CENTER);
+            this.add(heading, BorderLayout.NORTH);
+    
+            //  working with jpanel
+    
+            mainPanel = new JPanel();
+    
+            // setting layout on main panel
+            mainPanel.setLayout(new GridLayout(3,2));
+    
+            nameLabel = new JLabel("Enter Name: ");
+            nameLabel.setFont(font);
+    
+            passwordLabel = new JLabel("Enter Password: ");
+            passwordLabel.setFont(font);
 
-    public Window()
-    {
-        super.setTitle("Form Using Java Jrame");
-        super.setSize(400, 400);
-        super.setLocation(600, 50);
-       
-        this.createGUI();
+            passwordField = new JPasswordField();
+            passwordField.setFont(font);
+    
+            nameTextField = new JTextField();
+            nameTextField.setFont(font);
+    
+            button1 = new JButton("Submit");
+            button1.setFont(font);
+            button2 = new JButton("Reset");
+            button2.setFont(font);
+    
+            mainPanel.add(nameLabel);
+            mainPanel.add(passwordField);
+            mainPanel.add(nameTextField);
+            mainPanel.add(button1);
+            mainPanel.add(button2);
 
-        super.setVisible(true);
-    }
+        this.add(mainPanel,BorderLayout.CENTER);
 
-    public void createGUI(){
 
-        this.setLayout(new BorderLayout());
-        heading=new JLabel("First Form...");
-        heading.setFont(font);
-        heading.setHorizontalAlignment(JLabel.CENTER);
-        this.add(heading, BorderLayout.NORTH);
     }
 }
